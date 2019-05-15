a1ed4adc429a87acf4a341307f3cb3018d89676c
technologypilot <ike.x.kim@gmail.com>
2019-05-15 15:36:27 -0400

    add seeder

---
+++ seed.digital.groovy
@@ -0,0 +1,18 @@
+job('Infra-Provisioning') {
+    scm {
+        git('https://github.com/technologypilot/dsl-test.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        echo ike
+    }
+}
