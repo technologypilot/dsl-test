cb0679b4473420ef883b6c049bc3d33c16d6cf7a
technologypilot <ike.x.kim@gmail.com>
2019-05-20 00:54:36 -0400

    change

--- create-pipeline.sh
+++ create-pipeline.sh
@@ -4,7 +4,7 @@ scm_username=$1
 scm_password=$2
 echo $1
 echo $2
-# python3.7 -m venv test
-# source test/bin/activate
-# python3.7 -m pip install -r pip-requirements.txt
-# python3.7 create-pipeline.py --scm-username $scm_username --scm-password $scm_password
+python3.7 -m venv test
+source test/bin/activate
+python3.7 -m pip install -r pip-requirements.txt
+python3.7 create-pipeline.py --scm-username $scm_username --scm-password $scm_password
