0fcc3ba738267d0436e2d724a30b55367c337bb4
technologypilot <ike.x.kim@gmail.com>
2019-05-19 21:28:42 -0400

    add scripts

---
+++ create-pipeline.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+scm_username=$1
+scm_password=$2
+echo $1
+echo $2
+# python3.7 -m venv test
+# source test/bin/activate
+# python3.7 -m pip install -r pip-requirements.txt
+# python3.7 create-pipeline.py --scm-username $scm_username --scm-password $scm_password
